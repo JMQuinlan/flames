c2c77bfd6045d88fa23dd5d4ce7c5afecd9c8057
TylerT11 <tryontj03@gmail.com>
2025-11-07 20:48:00 -0700

    Static test case

--- src/Integrator/Hydro2.cpp
+++ src/Integrator/Hydro2.cpp
@@ -1256,13 +1256,13 @@ void Hydro2::Advance(int lev, Set::Scalar time, Set::Scalar dt)
             // Source(i, j, k, 3) = Source(i, j, k, 3) - E_vol(i, j, k) * deta_dt;
 
             // DEBUGGING:
-            rho_flux(i, j, k) = (flux_xlo.mass - flux_xhi.mass) / (DX[0] + small) + (flux_ylo.mass - flux_yhi.mass) / (DX[1] + small);
-            M_flux(i, j, k, 0) = (flux_xlo.momentum_normal - flux_xhi.momentum_normal) / (DX[0] + small) + (flux_ylo.momentum_tangent - flux_yhi.momentum_tangent) / (DX[1] + small);
-            M_flux(i, j, k, 1) = (flux_xlo.momentum_tangent - flux_xhi.momentum_tangent) / (DX[0] + small) + (flux_ylo.momentum_normal - flux_yhi.momentum_normal) / (DX[1] + small);
-            E_flux(i, j, k) = (flux_xlo.energy - flux_xhi.energy) / (DX[0] + small) + (flux_ylo.energy - flux_yhi.energy) / (DX[1] + small);
+            rho_flux(i, j, k) = (flux_xlo.mass - flux_xhi.mass) / (DX[0]) + (flux_ylo.mass - flux_yhi.mass) / (DX[1]);
+            M_flux(i, j, k, 0) = (flux_xlo.momentum_normal - flux_xhi.momentum_normal) / (DX[0]) + (flux_ylo.momentum_tangent - flux_yhi.momentum_tangent) / (DX[1]);
+            M_flux(i, j, k, 1) = (flux_xlo.momentum_tangent - flux_xhi.momentum_tangent) / (DX[0]) + (flux_ylo.momentum_normal - flux_yhi.momentum_normal) / (DX[1]);
+            E_flux(i, j, k) = (flux_xlo.energy - flux_xhi.energy) / (DX[0]) + (flux_ylo.energy - flux_yhi.energy) / (DX[1]);
 
             // Density
-            Set::Scalar drho_dt = (flux_xlo.mass - flux_xhi.mass) / (DX[0] + small) + (flux_ylo.mass - flux_yhi.mass) / (DX[1] + small) + Source(i, j, k, 0);
+            Set::Scalar drho_dt = (flux_xlo.mass - flux_xhi.mass) / (DX[0]) + (flux_ylo.mass - flux_yhi.mass) / (DX[1]) + Source(i, j, k, 0);
 
             rho_new(i, j, k) = rho(i, j, k) + (drho_dt)*dt;
             if (rho_new(i, j, k) < (0.0 + small)) {
@@ -1270,18 +1270,18 @@ void Hydro2::Advance(int lev, Set::Scalar time, Set::Scalar dt)
             }
 
             // Momentum
-            Set::Scalar dMx_dt = (flux_xlo.momentum_normal - flux_xhi.momentum_normal) / (DX[0] + small) + (flux_ylo.momentum_tangent - flux_yhi.momentum_tangent) / (DX[1] + small) + div_tau(0) +
+            Set::Scalar dMx_dt = (flux_xlo.momentum_normal - flux_xhi.momentum_normal) / (DX[0]) + (flux_ylo.momentum_tangent - flux_yhi.momentum_tangent) / (DX[1]) + div_tau(0) +
                                  //(mu * (lap_ux * eta(i, j, k))) +
                                  Source(i, j, k, 1);
             M_new(i, j, k, 0) = M(i, j, k, 0) + dMx_dt * dt;
 
-            Set::Scalar dMy_dt = (flux_xlo.momentum_tangent - flux_xhi.momentum_tangent) / (DX[0] + small) + (flux_ylo.momentum_normal - flux_yhi.momentum_normal) / (DX[1] + small) + div_tau(1) +
+            Set::Scalar dMy_dt = (flux_xlo.momentum_tangent - flux_xhi.momentum_tangent) / (DX[0]) + (flux_ylo.momentum_normal - flux_yhi.momentum_normal) / (DX[1]) + div_tau(1) +
                                  //(mu * (lap_uy * eta(i, j, k))) +
                                  Source(i, j, k, 2);
             M_new(i, j, k, 1) = M(i, j, k, 1) + dMy_dt * dt;
 
             // Energy
-            Set::Scalar dE_dt = (flux_xlo.energy - flux_xhi.energy) / (DX[0] + small) + (flux_ylo.energy - flux_yhi.energy) / (DX[1] + small) + Source(i, j, k, 3);
+            Set::Scalar dE_dt = (flux_xlo.energy - flux_xhi.energy) / (DX[0]) + (flux_ylo.energy - flux_yhi.energy) / (DX[1]) + Source(i, j, k, 3);
             if (Spec_Vol == 1)
             {
                 E_vol_new(i, j, k) = E_vol(i, j, k) + dE_dt * dt;
