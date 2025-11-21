41eda4b7b9cb9d4e2611f7d3a3078d0d7a9481f3
Tyler Tryon <tryontj03@gmail.com>
2025-11-20 23:15:49 -0700

    Removed Allen-Cahn for INCLINE Test due to eta nans

--- src/Integrator/Hydro2.cpp
+++ src/Integrator/Hydro2.cpp
@@ -1303,7 +1303,7 @@ void Hydro2::Advance(int lev, Set::Scalar time, Set::Scalar dt)
             //Set::Vector u_new = Set::Vector(M_new(i, j, k, 0) / (rho_new(i, j, k) + small), M_new(i, j, k, 1) / (rho_new(i, j, k) + small));
             //Set::Scalar deta_dt = -u_new.dot(grad_eta) + Mob * lap_eta; // Advection + diffusion // Maybe u new?
 
-            Set::Scalar Mob = 0.01 * DX[0] * DX[0];
+            Set::Scalar Mob = 0.0 //0.01 * DX[0] * DX[0];
 
             // Compute Laplacian of chemical potential (conservative form)
             Set::Scalar lap_mu_chem = Numeric::Laplacian(mu_chem_, i, j, k, 0, DX);
