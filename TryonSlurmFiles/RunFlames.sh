9626bec95c4c357a425074a5689e42f685d75c96
Tyler Tryon <tryontj03@gmail.com>
2026-06-14 23:12:53 -0600

    Modifed RPE Slurm

--- TryonSlurmFiles/RunFlames.sh
+++ TryonSlurmFiles/RunFlames.sh
@@ -7,37 +7,9 @@
 #SBATCH --ntasks-per-node=128
 #SBATCH -t 10:00:00
 
-#cd /mmfs1/home/ttryon/flames/bin/
+module load openmpi
 
-### OLD HYDRO ###
-#mpirun /home/ttryon/flames/bin/hydro-2d-g++ /home/ttryon/flames/tests/FlowRiemannShockTube/input
-
-#### HDYRO 2 ####
-
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowTwoPhase/input2
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowTwoPhase/LargeBubbleSigmaHigh
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowTwoPhase/LargeBubbleGammaHigh
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowTwoPhase/LargeBubbleNewKappa
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowTwoPhase/LargeBubbleOldKappa
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowTwoPhase/LargeBubbleOldKappaHighSigmaHighGamma
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowTwoPhase/LargeBubbleNewKappaHighSigmaHighGamma
-
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/LargeBubbleGammaHighTammann
-
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowTwoPhase/LargeBubbleGammaHighTammannSame
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowTwoPhase/RayleighTaylorInstability
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowRiemannUnitTests/input_AllaireAirWater
-
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowTwoPhase/LargeBubbleGammaHighTammannVapor
-
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowKellerMiksis/input_KellerMiksis_MildOscillation_NSCBC_Large
-
-srun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/Sch20_Oscillating_minmod_Neumann_Large
-
-# Couette Flow # 
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowCouette/input_hydro2
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowCouette/input_hydro2_2phase
-
-
-# Shock Droplet Interaction #
-#mpirun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowShockDroplet/RockULikeAHuricane
+#srun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/Sch20_Oscillating_Neumann
+#srun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/Sch20_Oscillating_Neumann_Large
+srun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/Sch20_Oscillating_NSCBC
+#srun /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/Sch20_Oscillating_NSCBC_Large
