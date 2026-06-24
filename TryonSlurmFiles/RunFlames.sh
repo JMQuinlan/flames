c9ba3580dff236c66a51e64ab2cbf526051fc5e3
Tyler Tryon <tryontj03@gmail.com>
2026-06-23 18:43:12 -0600

    Switched RPE to Neumann on bigmem paritition

--- TryonSlurmFiles/RunFlames.sh
+++ TryonSlurmFiles/RunFlames.sh
@@ -5,6 +5,7 @@
 #SBATCH -e /mmfs1/home/ttryon/FLAMES_out/err_flame_%j_stdout
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=128
+#SBATCH --partition=bigmem
 #SBATCH -t 22:00:00
 
 module purge
@@ -16,7 +17,7 @@ module load gnu9 mpich
 #srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-2d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/Sch20_Oscillating_NSCBC_Large
 
 # --- 3D variants (sphere bubble via x^2+y^2+z^2, AMR 64^3+L3, hydro2-3d-g++). ---
-#srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/Sch20_Oscillating_Neumann_3D
-srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/Sch20_Oscillating_NSCBC_3D
+srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/Sch20_Oscillating_Neumann_3D
+#srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/Sch20_Oscillating_NSCBC_3D
 #srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/Sch20_Collapsing_Neumann_3D
 #srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowRayleighPlesset/Sch20_Collapsing_NSCBC_3D
