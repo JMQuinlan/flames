ee5025b1a5748e48a12c8b0900c4c2544350d098
Tyler Tryon <tryontj03@gmail.com>
2026-09-09 17:50:07 -0600

    Driven bubble for lo slurm

--- TryonSlurmFiles/DrivenBubble/RunLowAmp.sh
+++ TryonSlurmFiles/DrivenBubble/RunLowAmp.sh
@@ -13,5 +13,5 @@ module load gnu9 mpich
 
 ## 3D Varients ##
 #srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowDrivenBubble/input_LowAmp
-#srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowDrivenBubble/input_LowAmp_NSCBC
-srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowDrivenBubble/input_LowAmp_NSCBC_Large
+srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowDrivenBubble/input_LowAmp_NSCBC
+#srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowDrivenBubble/input_LowAmp_NSCBC_Large
