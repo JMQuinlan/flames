b8c63310cad3580361e15bd5b78d8e9012799cc3
TylerT11 <tryontj03@gmail.com>
2026-09-16 01:29:54 -0600

    NSCBC sigma and lref LODI change for driven bubble - shrunk domain

--- TryonSlurmFiles/DrivenBubble/RunLowAmp.sh
+++ TryonSlurmFiles/DrivenBubble/RunLowAmp.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=LowAmp
 #SBATCH -o /mmfs1/home/ttryon/FLAMES_out/flame_%j_stdout
 #SBATCH -e /mmfs1/home/ttryon/FLAMES_out/err_flame_%j_stdout
-#SBATCH -N 2
+#SBATCH -N 1
 #SBATCH --ntasks-per-node=128
 #SBATCH --partition=compute-long
 #SBATCH -t 100:00:00
