11130d0e208f5fab5fe32661a5f5d921ca5641aa
TylerT11 <tryontj03@gmail.com>
2026-09-17 23:46:06 -0600

    Slurm files

--- TryonSlurmFiles/DrivenBubble/Run_f40.8_A2.72.sh
+++ TryonSlurmFiles/DrivenBubble/Run_f40.8_A2.72.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=f81.5_A1.36
+#SBATCH --job-name=f40.8_A2.72
 #SBATCH -o /mmfs1/home/ttryon/FLAMES_out/flame_%j_stdout
 #SBATCH -e /mmfs1/home/ttryon/FLAMES_out/err_flame_%j_stdout
 #SBATCH -N 1
@@ -12,4 +12,4 @@ module purge
 module load gnu9 mpich
 
 ## 3D Varients ##
-srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowDrivenBubble/input_f81.5_A1.36
+srun --mpi=pmi2 /home/ttryon/flames/bin/hydro2-3d-g++ /home/ttryon/flames/tests/FlowDrivenBubble/input_f40.8_A2.72
